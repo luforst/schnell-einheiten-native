f5924143eedf460e9a200cbe75840381a661af9a
Lukas Forst <lukas.forst@zoho.com>
2018-11-23 16:05:35 +0100

    Add passing down of quizData from App to QuizTableCC

--- App.js
+++ App.js
@@ -69,7 +69,7 @@ export default class App extends React.Component {
 		return (
 			<View style={styles.container}>
 				<Text>Open up App.js to start working on your app!</Text>
-				<QuizTable />
+				<QuizTable quizData={this.state.quizData} />
 				<Button onPress={this.refreshContent} title="Neue Inhalte laden" />
 			</View>
 		);
