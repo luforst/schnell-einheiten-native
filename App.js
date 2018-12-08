deab16645608e37406db850065b23754f1f6862a
Lukas Forst <lukas.forst@zoho.com>
2018-12-08 22:24:01 +0100

    Remove obsolete component imports

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { StyleSheet, Button, Switch } from 'react-native';
+import { StyleSheet } from 'react-native';
 import { Container, Header, Content, Card, CardItem, Body, Text, View, DeckSwiper } from 'native-base';
 import './contentdb.js'
 
