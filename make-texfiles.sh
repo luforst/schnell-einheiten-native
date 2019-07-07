2ed3052a2b94a653abab9bb93227735961ea0877
Lukas Forst <lukas.forst@zoho.com>
2019-07-07 10:14:11 +0200

    Restructure into for loop

--- make-texfiles.sh
+++ make-texfiles.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 python3 generate-texfiles.py contentdb.json
 cd assets/formula
-pdflatex -synctex=1 -interaction=nonstopmode -shell-escape *.tex
-pdf2svg *.pdf
+for texfile in *.tex
+do
+    pdflatex -synctex=1 -interaction=nonstopmode -shell-escape $texfile
+    pdf2svg *.pdf
+    rm *.pdf
+done
 #rm *.{pdf,tex,aux,synctex,log}
