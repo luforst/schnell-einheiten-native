0d823e4f4c765fa85b2f3fed23fbcec8896e7d63
Lukas Forst <lukas.forst@zoho.com>
2019-07-07 11:15:17 +0200

    Replace wildcard file names with $(); make it work

--- make-texfiles.sh
+++ make-texfiles.sh
@@ -4,7 +4,6 @@ cd assets/formula
 for texfile in *.tex
 do
     pdflatex -synctex=1 -interaction=nonstopmode -shell-escape $texfile
-    pdf2svg *.pdf
-    rm *.pdf
+    pdf2svg $(echo $texfile | sed 's/tex/pdf/') $(echo $texfile | sed 's/tex/svg/')
 done
-#rm *.{pdf,tex,aux,synctex,log}
+rm *.{pdf,tex,aux,synctex,log}
